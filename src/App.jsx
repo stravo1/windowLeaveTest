9027bd26a40ee24ba20e55f7287ae6f936a3510b
stravo1@msi <stravoone@gmail.com>
2023-09-15 23:28:49 +0530

    add: detect visibilitychange

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,13 @@ function App() {
       e.returnValue = confirmationMessage;
       return confirmationMessage;
     });
+    document.addEventListener("visibilitychange", (e) => {
+      if(document.visibilityState == "hidden"){
+        alert("Did you just change tabs? Or maybe minimized the browser?")
+      }
+    });
     console.log(
-      "https://stackoverflow.com/questions/38879742/is-it-possible-to-display-a-custom-message-in-the-beforeunload-popup"
+      "Why can't we display custom messages on confirmation dialogue: https://stackoverflow.com/questions/38879742/is-it-possible-to-display-a-custom-message-in-the-beforeunload-popup"
     );
     setInterval(() => {
       setCount((arg) => arg + 0.5);
